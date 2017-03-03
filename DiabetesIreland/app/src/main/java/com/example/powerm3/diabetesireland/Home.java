c40ea4849c340a16ff234e16d713b09c5a1d9a6d
ConalCosgrove <cosgroco@tcd.ie>
2017-03-03 12:44:14 +0000

    Added basic home screen triangle and info page

--- DiabetesIreland/app/src/main/java/com/example/powerm3/diabetesireland/Home.java
+++ DiabetesIreland/app/src/main/java/com/example/powerm3/diabetesireland/Home.java
@@ -1,20 +1,40 @@
 package com.example.powerm3.diabetesireland;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
+
 
 public class Home extends AppCompatActivity {
 
     TextView welcomeLabel;
     User user;
+    ImageButton infoButton;
+    final Context context = this;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        welcomeLabel = (TextView) findViewById(R.id.topTextLabel);
+        infoButton = (ImageButton) findViewById(R.id.infoButton);
         user = (User) getIntent().getParcelableExtra("userData");
+
+        infoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context,Information.class);
+                startActivity(intent);
+                
+                overridePendingTransition(0, 0);
+            }
+        });
+
 
         //welcomeLabel.setText("Welcome" + user.getName());
 
---
+++ DiabetesIreland/app/src/main/java/com/example/powerm3/diabetesireland/Information.java
@@ -0,0 +1,13 @@
+package com.example.powerm3.diabetesireland;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Information extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information);
+    }
+}
