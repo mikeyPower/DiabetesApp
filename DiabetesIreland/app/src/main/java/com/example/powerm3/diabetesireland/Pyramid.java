d3eb623b0359a50e6b957ce07152144d38719997
ConalCosgrove <cosgroco@tcd.ie>
2017-03-07 13:08:41 +0000

    Added data persitence for food pyramid

--- DiabetesIreland/app/src/main/java/com/example/powerm3/diabetesireland/Pyramid.java
+++ DiabetesIreland/app/src/main/java/com/example/powerm3/diabetesireland/Pyramid.java
@@ -1,5 +1,7 @@
 package com.example.powerm3.diabetesireland;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +12,9 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 public class Pyramid extends AppCompatActivity {
+
+    SharedPreferences sharedPref;
+    SharedPreferences.Editor editor;
 
     TextView[] texts,foodTypeTexts;
     TextView badText,fatText,proteinText,dairyText,carbText,vegText;
@@ -25,6 +30,10 @@ public class Pyramid extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pyramid);
 
+        sharedPref =  getPreferences(Context.MODE_PRIVATE);
+        editor = sharedPref.edit();
+        //editor.clear();
+        //editor.commit();
         str = new String[] {"one","two","three","four","five","six"};
         maxes = new int[] {1,1,2,3,5,7};
         ptext = new int[6];
@@ -32,9 +41,7 @@ public class Pyramid extends AppCompatActivity {
         buttons = new ImageButton[6];
         foodTypeTexts = new TextView[6];
 
-        for(int i = 0;i < ptext.length;i++ ){
-            ptext[i] = 0;
-        }
+
 
         for(int i = 0; i < texts.length; i++){
             String name = "pyramid_" + str[i];
@@ -60,25 +67,77 @@ public class Pyramid extends AppCompatActivity {
                 }
             });
         }
-
         reset_pyramid();
-    }
+        for(int i = 0; i < ptext.length; i++) {
+            ptext[i] = sharedPref.getInt(foodTypes[i], 0);
+            update_label_nonAdd(i);
 
-    private void update_label(int id){
-        int newVal = ptext[id];
+        }
 
-        if(newVal < maxes[id]) {
-            foodArrays[id][newVal].setAlpha(0xFF);
-            newVal = ++ptext[id];
-            texts[id].setText(Integer.toString(newVal));
 
-            if(newVal == maxes[id]){
-                foodTypeTexts[id].setTextColor(0xFFFF0000);
-                buttons[id].setClickable(false);
-            }
+
+        for(int i = 0; i < ptext.length; i++) {
+            set_bar(i);
         }
     }
 
+    //Increments the number of a certain food group by one and updates the label on the left hand side as well as the bar
+    private void update_label(int id){
+        int newVal = ptext[id];
+        System.out.println("update label id = " + id + "newVal = " + newVal + " max = " + maxes[id]);
+        if(newVal < maxes[id]) {
+            foodArrays[id][newVal].setAlpha(0xFF);
+            newVal = ++ptext[id];
+            set_bar(id);
+            texts[id].setText(Integer.toString(newVal));
+            editor.putInt(foodTypes[id],ptext[id]);
+            editor.commit();
+
+        }
+        if(newVal == maxes[id]){
+            texts[id].setText(Integer.toString(newVal));
+            foodTypeTexts[id].setTextColor(0xFFFF0000);
+            buttons[id].setClickable(false);
+        }
+
+    }
+
+    //Updates one of the number labels on the left of the pyramid
+    private void update_label_nonAdd(int id){
+        int newVal = ptext[id];
+        System.out.println("update label id = " + id + "newVal = " + newVal + " max = " + maxes[id]);
+        if(newVal < maxes[id]) {
+            //foodArrays[id][newVal].setAlpha(0xFF);
+            set_bar(id);
+            texts[id].setText(Integer.toString(newVal));
+            editor.putInt(foodTypes[id],ptext[id]);
+            editor.commit();
+
+        }
+        if(newVal == maxes[id]){
+            texts[id].setText(Integer.toString(newVal));
+            foodTypeTexts[id].setTextColor(0xFFFF0000);
+            buttons[id].setClickable(false);
+        }
+
+    }
+
+    //Updates a certain bar of the pyramid
+    private void set_bar(int id){
+        String currentFood;
+
+        ImageView[] i = foodArrays[id];
+            currentFood = foodTypes[id];
+
+            for(int j = 0; j < foodArrays[id].length;j++){
+                foodArrays[id][j] = (ImageView) findViewById( (getResources().getIdentifier( (currentFood + j), "id", getPackageName() )) );
+                if(j < ptext[id]) {
+                    foodArrays[id][j].setAlpha(0xFF);
+                }
+            }
+
+    }
+    //Sets the pyramid to totally empty
     private void reset_pyramid(){
         veg = new ImageView[7];
         carb = new ImageView[5];
