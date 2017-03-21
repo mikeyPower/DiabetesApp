6cbc4fa5c025e178bfa63838573a259a7fdc5708
dormsby <dormsby@tcd.ie>
2017-03-21 12:49:55 +0000

    Pedometer added, still needs work

--- DiabetesIreland/app/src/main/java/com/example/powerm3/diabetesireland/Fitness.java
+++ DiabetesIreland/app/src/main/java/com/example/powerm3/diabetesireland/Fitness.java
@@ -4,21 +4,44 @@ package com.example.powerm3.diabetesireland;
  * Created by dormsby on 14/03/17.
  */
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.preference.PreferenceManager;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-public class Fitness extends AppCompatActivity {
+public class Fitness extends AppCompatActivity implements SensorEventListener  {
 
     SharedPreferences sharedPref;
     SharedPreferences.Editor editor;
+
+    private TextView textView;
+
+    private SensorManager mSensorManager;
+
+    private Sensor mStepCounterSensor;
+
+    private Sensor mStepDetectorSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fitness);
+
+        textView = (TextView) findViewById(R.id.steps);
+
+        mSensorManager = (SensorManager)
+                getSystemService(Context.SENSOR_SERVICE);
+        mStepCounterSensor = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        mStepDetectorSensor = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
 
 
     }
@@ -29,4 +52,43 @@ public class Fitness extends AppCompatActivity {
         super.onBackPressed();
         overridePendingTransition(0,0);
     }
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Sensor sensor = event.sensor;
+        float[] values = event.values;
+        int value = -1;
+
+        if (values.length > 0) {
+            value = (int) values[0];
+        }
+
+        if (sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+            textView.setText("Step Counter Detected : " + value);
+        } else if (sensor.getType() == Sensor.TYPE_STEP_DETECTOR) {
+            // For test only. Only allowed value is 1.0 i.e. for step taken
+            textView.setText("Step Detector Detected : " + value);
+        }
+    }
+
+    protected void onResume() {
+
+        super.onResume();
+
+        mSensorManager.registerListener(this, mStepCounterSensor,
+
+                SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(this, mStepDetectorSensor,
+
+                SensorManager.SENSOR_DELAY_FASTEST);
+
+    }
+
+    protected void onStop() {
+        super.onStop();
+        mSensorManager.unregisterListener(this, mStepCounterSensor);
+        mSensorManager.unregisterListener(this, mStepDetectorSensor);
+    }
+    //so the class doesn't have to be abstract
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy){}
 }
